3a06e61bdd991e288fdffcbd15aff75abb81103b
Sakshi Tiwari <93910804+Sbt07@users.noreply.github.com>
2023-10-07 11:32:45 +0530

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,11 +1,11 @@
 const recordBtn = document.querySelector(".record"),
   result = document.querySelector(".result"),
-  downloadBtn = document.querySelector(".download"),
+  copyBtn = document.querySelector(".copy"), // Add a copy button
   inputLanguage = document.querySelector("#language"),
   clearBtn = document.querySelector(".clear");
 
 let SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition,
+  window.SpeechRecognition || window.webkitSpeechRecognition,
   recognition,
   recording = false;
 
@@ -30,21 +30,21 @@ function speechToText() {
     recognition.start();
     recognition.onresult = (event) => {
       const speechResult = event.results[0][0].transcript;
-      //detect when intrim results
+      // detect when interim results
       if (event.results[0].isFinal) {
         result.innerHTML += " " + speechResult;
         result.querySelector("p").remove();
       } else {
-        //creative p with class interim if not already there
+        // create p with class interim if not already there
         if (!document.querySelector(".interim")) {
           const interim = document.createElement("p");
           interim.classList.add("interim");
           result.appendChild(interim);
         }
-        //update the interim p with the speech result
+        // update the interim p with the speech result
         document.querySelector(".interim").innerHTML = " " + speechResult;
       }
-      downloadBtn.disabled = false;
+      copyBtn.disabled = false;
     };
     recognition.onspeechend = () => {
       speechToText();
@@ -54,9 +54,7 @@ function speechToText() {
       if (event.error === "no-speech") {
         alert("No speech was detected. Stopping...");
       } else if (event.error === "audio-capture") {
-        alert(
-          "No microphone was found. Ensure that a microphone is installed."
-        );
+        alert("No microphone was found. Ensure that a microphone is installed.");
       } else if (event.error === "not-allowed") {
         alert("Permission to use microphone is blocked.");
       } else if (event.error === "aborted") {
@@ -67,7 +65,6 @@ function speechToText() {
     };
   } catch (error) {
     recording = false;
-
     console.log(error);
   }
 }
@@ -88,25 +85,20 @@ function stopRecording() {
   recording = false;
 }
 
-function download() {
+function copyToClipboard() {
   const text = result.innerText;
-  const filename = "speech.txt";
-
-  const element = document.createElement("a");
-  element.setAttribute(
-    "href",
-    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
-  );
-  element.setAttribute("download", filename);
-  element.style.display = "none";
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+  const tempTextarea = document.createElement("textarea");
+  tempTextarea.value = text;
+  document.body.appendChild(tempTextarea);
+  tempTextarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempTextarea);
+  alert("Text copied to clipboard!");
 }
 
-downloadBtn.addEventListener("click", download);
+copyBtn.addEventListener("click", copyToClipboard);
 
 clearBtn.addEventListener("click", () => {
   result.innerHTML = "";
-  downloadBtn.disabled = true;
+  copyBtn.disabled = true;
 });
